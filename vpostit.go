c3c8419a73827491ae7ee58f3b20d8a5864501aa
borkab <borbala.botond@gmail.com>
2022-12-15 19:49:50 +0100

    def. repository with the 4 funcs

--- vpostit.go
+++ vpostit.go
@@ -1,5 +1,7 @@
 package vpostit
 
+import "context"
+
 // Note represents an online post-it
 type Note struct {
 	Title string //title of your note
@@ -11,4 +13,11 @@ type Note struct {
 type Info struct {
 	MadeDay    string //date of the birthday of your note
 	LastChange string //last change of your note
+}
+
+type Repository interface {
+	Create(context.Context, *Note) error
+	Update(context.Context, *Note) error
+	FindByID(context.Context, NoteID) (_ Note, found bool, _ error)
+	DeletByID(context.Context, NoteID) error
 }
