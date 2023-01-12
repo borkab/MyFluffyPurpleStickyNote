d170ee915dea5d08c5590b621678df631b471e34
borkab <borbala.botond@gmail.com>
2023-01-12 23:37:23 +0100

    test fails but where

--- handler.go
+++ handler.go
@@ -5,7 +5,6 @@
 package vpostit
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 )
@@ -16,24 +15,24 @@ type FluffyHandler struct { //create a handler struct
 
 // implement `ServeHTTP` method on `FluffyHandler` struct
 func (f *FluffyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	/*
-		//create response binary data
-		data := &FluffyHandler{
-			Fluff: []byte("Hello, World!"),
+
+	//create response binary data
+	data := []byte("Hello World! <3")
+
+	//write `data` to response
+	res.Write(data)
+
+	if req.Method == "POST" { //ha a POST metodust hivom meg rajta,
+		bs, err := io.ReadAll(req.Body) //akkor olvassa be a keres Body tartalmat
+		if err != nil {                 //ha vmi hiba van, kuldje vissza
+			res.WriteHeader(http.StatusBadRequest) //ezt a statusz kodot
+			return                                 //.
 		}
-
-		//write `data` to response
-		res.Write(data.Fluff)
-	*/
-
-	//to see which methods can write a string to a Writer
-
-	//write `Hello` using `io.WriteString` function
-	io.WriteString(res, "Hello ")
-
-	//write `World` using `fmt.Fprint` function
-	fmt.Fprint(res, "World! ")
-
-	//write `<3` using simple `Write` call
-	res.Write([]byte("<3"))
+		f.Fluff = bs         //a handlerunkre mutato pointer strukturank Fluff fieldjenek tartalma egyezzen meg a bs bajt slicesszal
+		res.WriteHeader(418) //dobja vissza ezt a valasz kodot: I am a tea pot
+	}
+	if req.Method == "GET" { //ha a GET metodust hivom meg rajta
+		res.WriteHeader(http.StatusOK) //dobja vissza ezt a statusz kodot
+		res.Write(f.Fluff)             //dobja vissza valaszkent a handlerunkre mutato pointer struct Fluff fieldjenek tartalmat
+	}
 }
--- handler_test.go
+++ handler_test.go
@@ -1,7 +1,12 @@
 package vpostit
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -12,4 +17,43 @@ func ErrCheck(err error) {
 }
 func TestHandler(t *testing.T) {
 
+	handler := &FluffyHandler{}           //az en handler structom
+	server := httptest.NewServer(handler) //nyitok egy servert es beledobom a handleremet
+	defer server.Close()                  //ha minden kesz, bezarom a servert
+
+	t.Run("", func(t *testing.T) {
+		const expected = "Hello World! <3"         //ezt szeretnem a request Bodyban
+		requestURL := server.URL + "/hello"        //a port URL cime amin a server figyel /hello-val a vegen
+		requestBody := strings.NewReader(expected) //megadom h a request Body beolvassa az elvart szoveget
+
+		//inditok egy uj kerest, amiben megadom, h milyen metodust- hivok meg rajta, milyen cimen es mi legyen a tartalma
+		req, err := http.NewRequest(http.MethodPost, requestURL, requestBody)
+		ErrCheck(err)
+
+		//
+		resp, err := http.DefaultClient.Do(req) //ez az alap kliensunk, aminek megadjuk a keresunket
+		fmt.Println(resp)                       //kiiratjuk a valaszt(response obbjektum)
+		ErrCheck(err)
+
+		if http.StatusCreated != resp.StatusCode {
+			t.Fatal("created code doen't as expected. it should be: 201")
+		} //ez az elvarasunk a servertol
+
+		req, err = http.NewRequest(http.MethodGet, requestURL, nil)
+		ErrCheck(err)
+
+		resp, err = http.DefaultClient.Do(req)
+		ErrCheck(err)
+
+		if http.StatusOK != resp.StatusCode {
+			t.Fatal("created code doesn't as expected. it should be: 201")
+		}
+		bs, err := io.ReadAll(resp.Body)
+		ErrCheck(err)
+
+		if strings.TrimSpace(string(bs)) != expected {
+			t.Fatal("oh fluff it! :( ")
+		}
+
+	})
 }
