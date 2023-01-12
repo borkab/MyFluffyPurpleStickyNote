0942702b6286df1ef9b08b43e5edc027da0a04db
borkab <borbala.botond@gmail.com>
2023-01-12 09:24:20 +0100

    to see which methods can write a string to a Writer

--- handler.go
+++ handler.go
@@ -5,6 +5,8 @@
 package vpostit
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -14,12 +16,24 @@ type FluffyHandler struct { //create a handler struct
 
 // implement `ServeHTTP` method on `FluffyHandler` struct
 func (f *FluffyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	/*
+		//create response binary data
+		data := &FluffyHandler{
+			Fluff: []byte("Hello, World!"),
+		}
 
-	//create response binary data
-	data := &FluffyHandler{
-		Fluff: []byte("Hello, World!"),
-	}
+		//write `data` to response
+		res.Write(data.Fluff)
+	*/
 
-	//write `data` to response
-	res.Write(data.Fluff)
+	//to see which methods can write a string to a Writer
+
+	//write `Hello` using `io.WriteString` function
+	io.WriteString(res, "Hello ")
+
+	//write `World` using `fmt.Fprint` function
+	fmt.Fprint(res, "World! ")
+
+	//write `<3` using simple `Write` call
+	res.Write([]byte("<3"))
 }
