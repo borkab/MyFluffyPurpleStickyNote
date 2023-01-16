143e6131b5c72ce823863b14f5652ae7bd588bfb
Adam Luzsi <adamluzsi@gmail.com>
2023-01-16 14:07:45 +0100

    wip

--- handler.go
+++ handler.go
@@ -14,25 +14,7 @@ type FluffyHandler struct { //create a handler struct
 
 // implement `ServeHTTP` method on `FluffyHandler` struct
 func (f *FluffyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Allow", "POST")
-	//res.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	res.WriteHeader(418)
-	res.Write([]byte("Hello World\n"))
-	/*
-		code, err := res.Write([]byte("Hello World\n"))
-		if code != http.StatusTeapot {
-			log.Fatal("statuscode is not as expected")
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-	*/
-	//res.WriteHeader(http.StatusTeapot) //dobja vissza ezt a valasz kodot: I am a tea pot
-
-	/*
-		if req.Method == "GET" { //ha a GET metodust hivom meg rajta
-			res.WriteHeader(http.StatusOK) //dobja vissza ezt a statusz kodot
-			res.Write(f.Fluff)             //dobja vissza valaszkent a handlerunkre mutato pointer struct Fluff fieldjenek tartalmat
-		}
-	*/
+	res.Header().Set("X-Foo", "bar")
+	res.WriteHeader(http.StatusTeapot)
+	_, _ = res.Write([]byte("Hello World\n"))
 }
--- handler_test.go
+++ handler_test.go
@@ -1,53 +1,31 @@
 package vpostit
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 
 	"github.com/adamluzsi/testcase/assert"
 )
 
 func TestHandler(t *testing.T) {
-
 	handler := &FluffyHandler{}           //az en handler structom
 	server := httptest.NewServer(handler) //nyitok egy servert es beledobom a handleremet
 	defer server.Close()                  //ha minden kesz, bezarom a servert
 
-	const expected = "Hello World! <3"         //ezt szeretnem a request Bodyban
-	requestURL := server.URL + "/hello"        //a port URL cime amin a server figyel /hello-val a vegen
-	requestBody := strings.NewReader(expected) //megadom h a request Body beolvassa az elvart szoveget
-
 	//inditok egy uj kerest, amiben megadom, h milyen metodust- hivok meg rajta, milyen cimen es mi legyen a tartalma
-	req, err := http.NewRequest(http.MethodPost, requestURL, requestBody)
+	req, err := http.NewRequest(http.MethodPost, server.URL, nil)
 	assert.NoError(t, err)
 
-	//
-	resp, err := http.DefaultClient.Do(req) //ez az alap kliensunk, aminek megadjuk a keresunket
-	fmt.Println(resp)                       //kiiratjuk a valaszt(response obbjektum)
+	response, err := server.Client().Do(req)
 	assert.NoError(t, err)
+	assert.Equal(t, http.StatusTeapot, response.StatusCode)
+	assert.Equal(t, "bar", response.Header.Get("X-Foo"))
 
-	if http.StatusTeapot != resp.StatusCode {
-		t.Fatal("created code doesn't as expected. it should be: 418")
-	} //ez az elvarasunk a servertol
-
-	//req, err = http.NewRequest(http.MethodGet, requestURL, nil)
-	//assert.NoError(t, err)
-
-	resp, err = http.DefaultClient.Do(req)
+	bs, err := io.ReadAll(response.Body)
 	assert.NoError(t, err)
-
-	if http.StatusOK != resp.StatusCode {
-		t.Fatal("created code doesn't as expected. it should be: 201")
-	}
-	bs, err := io.ReadAll(resp.Body)
-	assert.NoError(t, err)
-
-	if strings.TrimSpace(string(bs)) != expected {
-		t.Fatal("oh fluff it! :( ")
-	}
-
+	assert.NotEmpty(t, bs)
+	assert.NoError(t, response.Body.Close())
+	assert.Contain(t, string(bs), "Hello World! <3")
 }
