a5940deff2b568e0974eae2c3a20fbc3f28f5bcb
borkab <borbala.botond@gmail.com>
2023-01-28 14:08:28 +0100

    .

--- json.go
+++ json.go
@@ -2,9 +2,7 @@ package vpostit
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"reflect"
 )
 
 type ExampleStruct struct {
@@ -20,20 +18,14 @@ func errCheck(err error) error {
 	return err
 }
 
-func JJson(ent, ent2 ExampleStruct) (ExampleStruct, bool, error) {
-	//ent := ExampleStruct{}
-	//ent2 := ExampleStruct{}
-	equal := !false
+func JJson(ent, ent2 ExampleStruct) ExampleStruct {
+
 	marshaledEnt, err := json.Marshal(ent) //a marshalledEnt valtozonak megadom ertekkent azt a []byte-t, ami az ent nevu struct json.Marshal func utan jott letre
 	errCheck(err)
 	stringedME := string(marshaledEnt) //a marhalozas utan letrejott []byte-t konvertalom stringge, es ezt a stringet megadom a stringedME valtozo ertekenek
 
 	json.Unmarshal([]byte(stringedME), &ent2) //az unmarshal func-nek megadom parameternek az igy mar stringge alakitott []byte-t, es egy a korabban letrehozott ent2 structra mutato pointert
 	errCheck(err)
-	if !reflect.DeepEqual(ent, ent2) {
-		equal = false
-		err = errors.New("schade marmelade")
-	}
 
-	return ent2, equal, err
+	return ent2
 }
